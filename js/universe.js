26a6b51837207a4f3a254e9241ff351098403389
saiaashishntrinetra-dotcom <saiaashishntrinetra@gmail.com>
2026-08-31 02:39:52 +0530

    Update universe.js

--- js/universe.js
+++ js/universe.js
@@ -1,1 +1,37 @@
+/* =========================================
+   STAR FIELD
+========================================= */
 
+const starContainer = document.getElementById("stars");
+
+function createStars(amount = 180) {
+
+    for (let i = 0; i < amount; i++) {
+
+        const star = document.createElement("div");
+
+        star.style.position = "absolute";
+        star.style.width = "2px";
+        star.style.height = "2px";
+
+        star.style.borderRadius = "50%";
+
+        star.style.background = "white";
+
+        star.style.left =
+            Math.random() * 100 + "%";
+
+        star.style.top =
+            Math.random() * 100 + "%";
+
+        star.style.opacity =
+            Math.random() * 0.7 + 0.2;
+
+        star.style.transform =
+            `scale(${Math.random() * 1.5 + 0.5})`;
+
+        starContainer.appendChild(star);
+    }
+}
+
+createStars();
